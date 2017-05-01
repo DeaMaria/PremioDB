7d53afc8a893348cebf01bc505feee8bc7c3afc2
mpl96 <mpl_96@hotmail.es>
2017-05-01 20:07:12 +0200

    formularios de registro
    Signed-off-by: mpl96 <mpl_96@hotmail.es>

--- PremDB/insColegio.php
+++ PremDB/insColegio.php
@@ -18,10 +18,10 @@ $compromiso=$_POST['compromiso'];
 
 
 // Insertar el registro en la Tabla
-$sql="INSERT INTO entidades(CIF, nom_entidad, responsable, telefono, idLocalidad, domicilio_entidad, correo, web, estatutos, resgistros, compromiso) VALUES ('$cif', '$nom_entidad', '$respon', $telf, $idLoc, '$dom_enti', '$correo', '$web', '$estatutos', '$resgistros', '$compromiso')";
+$sql="INSERT INTO colegios(CIF, nom_entidad, responsable, telefono, idLocalidad, domicilio_entidad, correo, web, estatutos, resgistros, compromiso) VALUES ('$cif', '$nom_entidad', '$respon', $telf, $idLoc, '$dom_enti', '$correo', '$web', '$estatutos', '$resgistros', '$compromiso')";
 
 mysqli_query($conexion, $sql) or die ("Error en la consulta $sql");
 mysqli_close($conexion);
 
-header("location:formEnti.php");
+header("location:formColegio.php");
 ?>
